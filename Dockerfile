94b182291927f94c322ecb343d38e9806ac38e18
sintecs <s1ntecs@icloud.com>
2025-07-21 23:41:04 +0500

    step 1

---
+++ Dockerfile
@@ -0,0 +1,49 @@
+FROM nvidia/cuda:12.4.1-cudnn-devel-ubuntu22.04
+
+ENV DEBIAN_FRONTEND=noninteractive \
+    PYTHONUNBUFFERED=1 \
+    PIP_NO_CACHE_DIR=off \
+    SHELL=/bin/bash
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+
+# 1) Системные зависимости
+RUN apt update && \
+    apt install -y --no-install-recommends \
+      python3-dev python3-pip python3.10-venv \
+      fonts-dejavu-core git git-lfs jq wget curl \
+      libglib2.0-0 libsm6 libgl1 libxrender1 libxext6 \
+      ffmpeg procps && \
+    rm -rf /var/lib/apt/lists/*
+RUN git lfs install
+
+WORKDIR /workspace
+
+# 2) Копируем файл зависимостей
+COPY requirements.txt .
+
+# 3) Устанавливаем PyTorch, torchvision, torchaudio и xFormers
+RUN pip3 install --upgrade pip && \
+    pip3 install \
+      torch==2.4.0 torchvision==0.19.0 \
+        --index-url https://download.pytorch.org/whl/cu124
+    # pip3 install \
+    #   xformers==0.0.28.post1 --index-url https://download.pytorch.org/whl/cu124
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+RUN curl -o /usr/local/bin/pget -L "https://github.com/replicate/pget/releases/download/v0.6.0/pget_linux_x86_64" && \
+    chmod +x /usr/local/bin/pget
+
+RUN pip install onnxruntime-gpu \
+      --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
+
+
+# 4) Копируем остальной код
+COPY . .
+
+# 5) Создаём каталоги и загружаем чекпоинты
+RUN mkdir -p loras checkpoints && \
+    python3 download_checkpoints.py
+
+# 6) Точка входа
+COPY --chmod=755 start_standalone.sh /start.sh
+ENTRYPOINT ["/start.sh"]
--- download_checkpoints.py
+++ download_checkpoints.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 import os
+
+from helpers.comfyui import ComfyUI
 from huggingface_hub import hf_hub_download
 
-from  styles import STYLE_URLS_UNIQUE
+REPO_ID = "frankjoshua/albedobaseXL_v21"
 
-# Репозиторий на Hugging Face
-REPO_ID = "Comfy-Org/Wan_2.1_ComfyUI_repackaged"
-
-# Список файлов для скачивания: (путь внутри репозитория, локальная папка для сохранения)
 FILES_TO_DOWNLOAD = [
-    # Уже существующий файл из примера
-    ("split_files/diffusion_models/wan2.1_i2v_480p_14B_fp16.safetensors", "ComfyUI/models/diffusion_models"),
-    # Новые файлы для ComfyUI
-    ("split_files/text_encoders/umt5_xxl_fp16.safetensors",   "ComfyUI/models/text_encoders"),
-    ("split_files/vae/wan_2.1_vae.safetensors",               "ComfyUI/models/vae"),
-    ("split_files/clip_vision/clip_vision_h.safetensors",     "ComfyUI/models/clip_vision"),
+    ("albedobaseXL_v21.safetensors", "ComfyUI/models/checkpoints"),
 ]
 
-def download_wan_files():
+
+def download_comfy():
+    comfyUI = ComfyUI("127.0.0.1:8188")
+    comfyUI.download_pre_start_models
+
+
+def download_sd_ckp():
     """
     Скачивает из репозитория на Hugging Face все файлы из списка FILES_TO_DOWNLOAD
     и сохраняет их в соответствующие папки внутри проекта ComfyUI.
@@ -39,26 +38,6 @@ def download_wan_files():
             print(f"Ошибка при скачивании {remote_path}: {e}")
 
 
-def download_loras():
-    downloaded_paths = []
-    for key, lora_url in STYLE_URLS_UNIQUE.items():
-        try:
-            parts = lora_url.split("/")
-            repo_id = "/".join(parts[3:5])
-            filename = parts[-1]
-            print(f"Downloading LoRA: key={key}, repo_id={repo_id}, filename={filename}")
-            local_path = hf_hub_download(
-                repo_id=repo_id,
-                filename=filename,
-                local_dir="./loras"
-            )
-            downloaded_paths.append(local_path)
-        except Exception as e:
-            print(f"Error downloading LoRA '{key}':", str(e))
-            # либо продолжаем (continue), либо прерываем полностью, в зависимости от задачи
-            raise RuntimeError(f"Failed to download LoRA '{key}': {str(e)}")
-    return downloaded_paths
-
-
 if __name__ == "__main__":
-    download_wan_files()
+    download_sd_ckp()
+    download_comfy()
